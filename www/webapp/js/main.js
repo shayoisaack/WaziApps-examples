9387ded2d4ccf293a6e188fc1bf5f556fdd02dd5
Isaack Shayo <shayoisaack@gmail.com>
2018-10-10 14:05:31 +0300

    minimize expand dashboard buttons, separate dashboard file

--- www/webapp/js/main.js
+++ www/webapp/js/main.js
@@ -2,5 +2,6 @@ $(function(){
     $('#dashboard-expand').on('click', function(){
         $('#dashboard-box').toggleClass('dashboard-box-full');
         $('#user').toggle();
+        $('.dashboard-expand').toggleClass('dashboard-minimize');
     });
 });
