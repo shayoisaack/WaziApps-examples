379a1912f1320870547a02d986055b65bc9308d8
Rafii Ramii <info.rafyramy@gmail.com>
2018-10-10 16:58:31 +0300

    android

---
+++ android/app/src/main/java/com/karibuyako/rafy/zenmovers/DriverInformation.java
@@ -0,0 +1,28 @@
+package com.karibuyako.rafy.zenmovers;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class DriverInformation extends Fragment {
+    public DriverInformation() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_driver_information, container, false);
+    }
+}
